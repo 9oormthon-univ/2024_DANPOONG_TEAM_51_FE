b872fd546b8701bf49a42135fb3bd895f807dfe4
kwonjeong <kwonjeong9470@gmail.com>
2024-11-18 17:46:07 +0900

    #1 setting: 메인 페이지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+import { ThemeProvider, styled } from 'styled-components';
+
+import GlobalStyle from './common/style/GlobalStyle';
+import theme from './common/style/theme';
+import { useLocation, Outlet } from 'react-router-dom';
+
+import TabBar from './page/main/component/TabBar';
+
+const hiddenPaths = ['/login', '/chatting/example']; // 예시 path
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { pathname } = useLocation();
+
+  const setScreenSize = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    const windowWidth =
+      window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const maxWidth = Math.min(393, windowWidth);
+    document.documentElement.style.setProperty('--app-max-width', `${maxWidth}px`);
+  };
+
+  useEffect(() => {
+    setScreenSize();
+    window.addEventListener('resize', setScreenSize);
+
+    return () => {
+      window.removeEventListener('resize', setScreenSize);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+        <Outlet />
+        {!hiddenPaths.includes(pathname) && (
+          <St.TabBarWrapper>
+            <TabBar />
+          </St.TabBarWrapper>
+        )} 
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
+
+const St = {
+  TabBarWrapper: styled.div`
+    display: flex;
+    overflow: hidden;
+    z-index: 1000;
+    width: 100%;
+    max-width: var(--app-max-width); 
+    height: 95px;
+    position: fixed;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    align-items: stretch;
+  `,
+}
---
+++ src/Router.tsx
@@ -0,0 +1,26 @@
+// Router.tsx
+import { Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import App from './App';
+import ChatPage from './page/main/chat/Chat';
+import HomePage from './page/main/home/Home';
+import MyPage from './page/main/mypage/Mypage';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="home" element={<HomePage />} />   
+            <Route path="chat" element={<ChatPage />} />
+            <Route path="mypage" element={<MyPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
