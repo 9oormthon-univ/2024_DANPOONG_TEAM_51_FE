e8261f75d991633c62cae4020e892a7db9392c39
kwonjeong <kwonjeong9470@gmail.com>
2024-11-20 01:26:29 +0900

    #7 chore: 채팅 페이지 라우터 임시 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,17 +7,19 @@ import ChatPage from './page/main/chat/Chat';
 import HomePage from './page/main/home/Home';
 import MyPage from './page/main/mypage/Mypage';
 import BtnTest from './page/index/BtnTest';
+import ChattingPage from '@main/chat/chatting/ChattingPage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Suspense>
         <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="home" element={<HomePage />} />   
-            <Route path="chat" element={<ChatPage />} />
-            <Route path="mypage" element={<MyPage />} />
-            <Route path="btnTest" element={<BtnTest />} />
+          <Route path='/' element={<App />}>
+            <Route path='home' element={<HomePage />} />
+            <Route path='chat' element={<ChatPage />} />
+            <Route path='mypage' element={<MyPage />} />
+            <Route path='btnTest' element={<BtnTest />} />
+            <Route path='chatting/example' element={<ChattingPage />} />
           </Route>
         </Routes>
       </Suspense>
