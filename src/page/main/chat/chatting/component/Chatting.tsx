c5e3acc1d70b5db143186797158b51ebad8e622f
kwonjeong <kwonjeong9470@gmail.com>
2024-11-20 22:57:19 +0900

    #7 chore: 불필요한 코드 제거

--- src/page/main/chat/chatting/component/Chatting.tsx
+++ src/page/main/chat/chatting/component/Chatting.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 import MentoringStart from '@main/chat/chatting/component/MentoringStart';
