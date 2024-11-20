c320459a597097e406ece8469b5df07ca4867865
GunderGie <rjs94951@gmail.com>
2024-11-21 04:09:01 +0900

    #11 style: Profile border 추가

--- src/page/component/Profile.tsx
+++ src/page/component/Profile.tsx
@@ -11,38 +11,47 @@ const Profile = ({
   src = defaultProfile
 }:ProfileProps) => {
   return (
-    <St.ProfileImg $size={size} src={src}/>
+    <St.ProfileWrapper $size={size}>
+      <St.ProfileImg src={src}/>
+    </St.ProfileWrapper>
   )
 }
 
 export default Profile;
 
 const St = {
-  ProfileImg: styled.img<{$size:ProfileProps["size"]}>`
+  ProfileWrapper: styled.div<{$size:ProfileProps["size"]}>`
+  border: 4px solid white;
+  border-radius: 50%;
+  background-color: white;
+  box-sizing: content-box;
+  ${(props) => {
+    switch (props.$size) {
+      case "md":
+        return`
+          width: 40px;
+          height: 40px;
+        `
+      case "lg":
+        return`
+          width: 48px;
+          height: 48px;
+        `
+      case "xl":
+        return`
+          width: 56px;
+          height: 56px;
+        `
+      case "2xl":
+        return`
+          width: 96px;
+          height: 96px;
+        `
+    }
+  }}
+`,
+  ProfileImg: styled.img`
     clip-path: circle();
-    ${(props) => {
-      switch (props.$size) {
-        case "md":
-          return`
-            width: 40px;
-            height: 40px;
-          `
-        case "lg":
-          return`
-            width: 48px;
-            height: 48px;
-          `
-        case "xl":
-          return`
-            width: 56px;
-            height: 56px;
-          `
-        case "2xl":
-          return`
-            width: 96px;
-            height: 96px;
-          `
-      }
-    }}
+    height: 100%;
   `,
 }
